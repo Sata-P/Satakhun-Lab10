37e39957d5016425483c21b615924b5c06654411
TABAKO <programhaha@gmail.com>
2023-12-21 21:12:36 +0700

    all done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:Satakhun Nontha
+# STUDENT ID:6606112158
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,11 +6,19 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
+
+    string cheer;
+	dest << "-------------------- BOOM ---------------------\n";
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	while(getline(source,cheer)){
+	
+	cout << cheer << '\n';
+	
+	}
+	
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
